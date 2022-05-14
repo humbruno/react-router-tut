4a1246853785d867d7fb165c223a802177615483
Hum Bruno <hsbruno1@gmail.com>
2022-05-14 18:56:57 +0100

    feat: sorting quotes and url updates

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,38 @@
+import { Fragment } from 'react';
+import { useParams, Route } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Max',
+    text: 'Learning React is fun!',
+  },
+  {
+    id: 'q2',
+    author: 'Maximilian',
+    text: 'Learning React is great!',
+  },
+];
+
 const QuoteDetail = () => {
-  return <h1>Quote Detail Page</h1>;
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={'/quotes/:quoteId/comments'}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
