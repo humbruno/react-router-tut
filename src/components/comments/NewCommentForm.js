7a6b480ebdf15ae38bbed44084d5b96502a92aa3
Hum Bruno <hsbruno1@gmail.com>
2022-05-15 15:39:29 +0100

    feat: add new comments to quote

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -24,7 +24,7 @@ const NewCommentForm = (props) => {
     // optional: Could validate here
 
     // send comment to server
-    sendRequest({ text: enteredText }, props.quoteId);
+    sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId });
   };
 
   return (
